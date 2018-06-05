3e6ca841948b6bbf350dc1439c4dd0ee89cc65ca
Daniel O'Brien <dannyk85.dob@gmail.com>
2018-06-05 23:59:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function destructivelyRemoveFirstKitten()
 // appends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged:
 function appendKitten(name)
 {
-  var retArray = kittens.slice(0)
+  var retArray = [...kittens, name]
   return retArray
 }
 
