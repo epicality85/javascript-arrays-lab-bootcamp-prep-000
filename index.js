cc7f106e236f1241f02d3781d15e6484f3bca771
Daniel O'Brien <dannyk85.dob@gmail.com>
2018-06-05 23:50:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 var kittens = ["Milo", "Otis", "Garfield"]
 
 // appends a kitten to the end of the kittens array:
+function destructivelyAppendKitten(name)
+{
+  kittens.push(name)
+  return kittens
+}
+
 
